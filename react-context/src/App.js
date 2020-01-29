1a2ff3355764403d039bbd8800d81d6e6336d26a
eternalarchive <unchd26@gmail.com>
2020-01-29 21:45:22 +0900

    feat: add context value change button

--- react-context/src/App.js
+++ react-context/src/App.js
@@ -1,18 +1,27 @@
-import React, { useState } from 'react';
-import Example1 from './components/Example1';
-import Example2 from './components/Example2';
-import Example3 from './components/Example3';
-import PersonContext from './contexts/PersonContext';
-import './App.css';
+import React, { useState } from "react";
+import Example1 from "./components/Example1";
+import Example2 from "./components/Example2";
+import Example3 from "./components/Example3";
+import PersonContext from "./contexts/PersonContext";
+import "./App.css";
 
 function App() {
   const [persons, setPersons] = useState([
-    { id: 0, name: 'Hee', age: 27 },
-    { id: 1, name: 'Jeong', age: 25 },
+    { id: 0, name: "Hee", age: 27 },
+    { id: 1, name: "Jeong", age: 25 }
   ]);
 
   return (
-    <PersonContext.Provider value={persons}>
+    <PersonContext.Provider
+      value={{
+        persons,
+        old: () => {
+          setPersons(persons =>
+            persons.map(person => ({ ...person, age: person.age + 1 }))
+          );
+        }
+      }}
+    >
       <div className="App">
         <header className="App-header">
           <Example1 />
